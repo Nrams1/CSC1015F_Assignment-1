4a7d0f9ea646f41aabcb7508a49ef5573b3cb48d
Neo Ramotlou <52963792+Nrams1@users.noreply.github.com>
2021-07-19 22:38:22 +0200

    Add files via upload

---
+++ time.py
@@ -0,0 +1,16 @@
+# The vality of a time
+# neo ramotlou
+# 02 march 2018
+
+hours = eval(input("Enter the hours: "))
+minutes = eval(input("Enter the minutes: "))
+seconds = eval(input("Enter the seconds: "))
+
+if hours < 0 or hours > 23:
+    print("Your time is invalid.")
+elif minutes < 0 or minutes > 59:
+    print("Your time is invalid.")
+elif seconds < 0 or seconds > 59:
+    print("Your time is invalid.")
+else:
+    print("Your time is valid.")
---
+++ triangle.py
@@ -0,0 +1,15 @@
+# area of a triangle
+# neo ramotlou
+# 04 march 2018
+
+import math
+a =eval(input("Enter the length of the first side: "))
+b =eval(input("Enter the length of the second side: "))
+c =eval(input("Enter the length of the third side: "))
+
+s =((a+b+c)/2 )
+area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+print("The area of the triangle with sides of length", a ,end=' and')
+print("",b ,end=' and')
+print("",c ,end=' is')
+print("",area ,end='.')
